efdbb64c5467809098f6a6d75d3406a6897419cd
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-01-29 01:04:36 +0000

    feat: adding tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Task.create(
+  title: "Configurar Sistema de Autenticação",
+  descriptions: "Implementar o sistema de login e registro para os três tipos de contas de usuário (basic, premium e premium plus"
+)
+Task.create(
+  title: "Criar Painel de Usuário",
+  descriptions: "Desenvolver um painel para que os usuários possam visualizar e gerenciar suas informações de conta, como status da assinatura, histórico de uso e configurações personalizadas."
+)
+Task.create(
+  title: "Implementar Sistema de Pagamento",
+  descriptions: "Configurar integração com um gateway de pagamento para possibilitar a assinatura de contas premium e premium plus, garantindo segurança e usabilidade."
+)
+Task.create(
+  title: "Testar Funcionalidades com Selenium",
+  descriptions: "Configurar integração com um gateway de pagamento para possibilitar a assinatura de contas premium e premium plus, garantindo segurança e usabilidade."
+)
+Task.create(
+  title: "Criar Página de Landing com React",
+  descriptions: "Desenvolver uma página de apresentação da aplicação com React e Tailwind, destacando os benefícios e diferenciais das contas premium e premium plus."
+)

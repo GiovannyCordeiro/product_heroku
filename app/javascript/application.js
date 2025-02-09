945d57c756e9c36d079ec2b8d52e85c5c56fd90f
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-02-09 14:19:06 +0000

    feat: defining pwa

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+function registerServiceWorker() {
+  console.log('Registering service worker...');
+
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service worker registered:', registration);
+    }).catch((error) => {
+      console.error('Service worker registration failed:', error);
+    });
+  });
+}
+
+if ('serviceWorker' in navigator) {
+  registerServiceWorker();
+}
